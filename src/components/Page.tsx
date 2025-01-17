5fe14bc1d4530c269d3da35442c8966a44d9b21b
Lucien Ramond <lucien_ramond@hotmail.com>
2025-01-17 23:05:14 +0100

    #3 Wiring register page

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -9,11 +9,14 @@ export default function Page({
   title?: string;
 }) {
   return (
-    <div className=" max-w-[90vw] bg-neutral-700 rounded-b-xl m-auto min-h-[80vh] px-4 pb-4">
-      <Typography variant="h5" className=" bg-neutral-800 rounded-b-xl p-2">
+    <div className=" max-w-[90vw] bg-neutral-700 border border-white border-t-0 rounded-b-xl m-auto min-h-[80vh] px-4 pb-4">
+      <Typography
+        variant="h5"
+        className=" border border-white border-t-0 bg-neutral-800 rounded-b-xl p-2"
+      >
         {title}
       </Typography>
-      <div className="py-2">{children}</div>
+      <div className="grid justify-center">{children}</div>
     </div>
   );
 }
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,3 +1,98 @@
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Typography,
+} from "@mui/material";
+import Page from "../../components/Page";
+import { useRef, useState } from "react";
+
+interface Form extends HTMLFormElement {
+  username: HTMLInputElement;
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+  password_check: HTMLInputElement;
+}
+
 export default function Register() {
-  return <div>Register</div>;
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const formRef = useRef<Form>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleForm = () => {
+    const form = formRef.current as Form;
+    if (form.password.value != form.password_check.value) {
+      setErrorMessage("Passwords must be the same");
+      return;
+    }
+    setErrorMessage(null);
+
+    fetch(`${BASE_URL}/user/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: form.username.value,
+        email: form.email.value,
+        password: form.password.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        data.message ? setErrorMessage(data.message) : console.log(data)
+      );
+  };
+
+  return (
+    <Page title="Register">
+      <form
+        ref={formRef}
+        className=" bg-white border border-black border-t-0 p-4 rounded-xl rounded-t-none grid gap-4 min-w-[500px] items-center"
+      >
+        <FormControl>
+          <InputLabel htmlFor="username">Name</InputLabel>
+          <OutlinedInput id="username" label="Name" />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <OutlinedInput id="email" label="Email" type="email" required />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <OutlinedInput
+            id="password"
+            label="Password"
+            type="password"
+            required
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="password_check">Repeat password</InputLabel>
+          <OutlinedInput
+            id="password_check"
+            label="Repeat password"
+            type="password"
+            required
+          />
+        </FormControl>
+        {errorMessage && (
+          <Typography className=" text-red-600 italic underline">
+            {errorMessage}
+          </Typography>
+        )}
+        <Button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            handleForm();
+          }}
+          variant="contained"
+        >
+          Register
+        </Button>
+      </form>
+    </Page>
+  );
 }
