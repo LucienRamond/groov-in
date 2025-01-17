e7f2a4f2b04e51a8007563a8873bfa3610a59bbc
Lucien Ramond <lucien_ramond@hotmail.com>
2025-01-17 11:43:33 +0100

    #1 Let's groove

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@mui/material";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [groove, setGroove] = useState(false);
   return (
-    <div className=" grid gap-2">
-      <h1>Goov'in</h1>
-      <Button variant="contained">Let's groove !</Button>
+    <div className={`grid gap-20`}>
+      <h1 className={`${groove && "animate-spin"}`}>Goov'in</h1>
+      <Button onClick={() => setGroove(!groove)} variant="contained">
+        Let's groove !
+      </Button>
     </div>
   );
 }
